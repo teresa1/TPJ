93b2b037861b611d1d35c5815ff7edc3807a12d9
teresa1 <teresasofialoureiro@hotmail.com>
2015-02-18 20:26:40 +0000

    wopwop

--- jogojogo/jogojogo/Game1.cs
+++ jogojogo/jogojogo/Game1.cs
@@ -1,12 +1,8 @@
 ﻿#region Using Statements
-using System;
-using System.Collections.Generic;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Storage;
-using Microsoft.Xna.Framework.GamerServices;
+
 #endregion
 
 namespace jogojogo
@@ -18,11 +14,19 @@ namespace jogojogo
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Texture2D box;
+        byte[,] board = new byte[22, 10];
+        byte[,] piece = {{0,1,0},{1,1,1}};
+        int pX = 4, pY = -2;
+        float lastMove = 0f;
 
         public Game1()
             : base()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.IsFullScreen = false;
+            graphics.PreferredBackBufferHeight = 600;
+            graphics.PreferredBackBufferWidth = 300;
             Content.RootDirectory = "Content";
         }
 
@@ -47,7 +51,7 @@ namespace jogojogo
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            box = Content.Load <Texture2D>("box");
             // TODO: use this.Content to load your game content here
         }
 
@@ -58,6 +62,7 @@ namespace jogojogo
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
+            box.Dispose();
         }
 
         /// <summary>
@@ -71,6 +76,12 @@ namespace jogojogo
                 Exit();
 
             // TODO: Add your update logic here
+            lastMove += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (lastMove > 1f && canGoDown())
+            {
+                pY++;
+                lastMove = 0;
+            }
 
             base.Update(gameTime);
         }
@@ -82,10 +93,34 @@ namespace jogojogo
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            
             // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 20; y++)
+                {
+                    if(board[y+2, x] != 0)
+                                spriteBatch.Draw(box, new Vector2(x*30, y*30), Color.White);
+                    if(y >= pY && x>=pX && y<pY +piece.GetLength(0) && x<pX + piece.GetLength(1))
+                         {
+                             if (piece[y - pY, x - pX] != 0)
+                                 spriteBatch.Draw(box, new Vector2(x*30, y*30), Color.White);
+                         }
+                }
+            }
+            spriteBatch.End();
 
             base.Draw(gameTime);
+
+        }
+
+        private bool canGoDown()
+        {
+            if(pY + piece.GetLength(0) >= 20)
+                return false;
+            else 
+                return true;
         }
     }
 }
