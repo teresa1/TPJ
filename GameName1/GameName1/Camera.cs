fd10ac68bf3b3ecbf86c0f6718f9305aa72ebf01
GilFerraz <gil.ferraz.gomes@outlook.com>
2015-04-11 02:51:12 +0100

    Adições à camara

--- GameName1/GameName1/Camera.cs
+++ GameName1/GameName1/Camera.cs
@@ -27,7 +27,7 @@ namespace GameName1
             Camera.worldWidth = worldWidth; 
         } 
 
-        // Define o alvo da camera
+        // Define o alvo da camera (centro)
         public static void SetTarget(Vector2 target) 
         { 
             Camera.target = target; 
@@ -42,7 +42,39 @@ namespace GameName1
                 Camera.ratio = Camera.gDevManager.PreferredBackBufferWidth / Camera.worldWidth; 
                 Camera.lastSeenPixelWidth = Camera.gDevManager.PreferredBackBufferWidth; 
              } 
-         } 
+        }
+
+        // Converte um ponto virtual (metros) para um ponto real (pixels)
+        public static Vector2 WorldPoint2Pixels(Vector2 point) 
+        { 
+            Camera.UpdateRatio(); 
+            Vector2 pixelPoint = new Vector2(); 
+
+            // Calcula os pixels em relação ao alvo da camara (centro) 
+            pixelPoint.X = (int)((point.X - target.X) * Camera.ratio + 0.5f); 
+            pixelPoint.Y = (int)((point.Y - target.Y) * Camera.ratio + 0.5f); 
+
+            // Proteta os pixels calculados para o canto inferior esquerdo do ecra 
+            pixelPoint.X += Camera.lastSeenPixelWidth / 2; 
+            pixelPoint.Y += Camera.gDevManager.PreferredBackBufferHeight / 2; 
+
+            // Inverter as coordenadas Y 
+            pixelPoint.Y = Camera.gDevManager.PreferredBackBufferHeight - pixelPoint.Y; 
+
+            return pixelPoint; 
+        } 
+
+        public static Rectangle WorldSize2PixelRectangle(Vector2 position, Vector2 size) 
+        { 
+            Camera.UpdateRatio(); 
+            Vector2 pixelPosition = WorldPoint2Pixels(position); 
+
+            int pixelWidth = (int)( size.X * Camera.ratio + .5f); 
+            int pixelHeight = (int)(size.Y * Camera.ratio + .5f); 
+
+            return new Rectangle((int)pixelPosition.X, (int)pixelPosition.Y, pixelWidth, pixelHeight); 
+        } 
+
 
         // Métodos get/set
         public static float WorldWidth
