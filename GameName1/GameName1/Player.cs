1351b842c93a311887585b7fb5b54a52f8392807
GilFerraz <gil.ferraz.gomes@outlook.com>
2015-04-11 16:58:41 +0100

    Mini

--- GameName1/GameName1/Player.cs
+++ GameName1/GameName1/Player.cs
@@ -71,25 +71,5 @@ namespace GameName1
             this.sourcePosition = position;
             this.direction = new Vector2((float)Math.Sin(rotation), (float)Math.Cos(rotation));
         }
-
-		//public Bullet(ContentManager cManager, Vector2 sourcePosition, float rotation) : base(cManager, "bullet")
-		//{
-		//    this.position = sourcePosition;
-		//    this.sourcePosition = sourcePosition;
-		//    this.rotation = rotation;
-		//    this.Scale(0.05f);
-		//    this.direction = new Vector2((float)Math.Sin(rotation),
-		//                                 (float)Math.Cos(rotation));
-		//}
-
-		//public override void Update(GameTime gameTime)
-		//{
-		//    position = position + direction * velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-		//    if ((position - sourcePosition).Length() > maxDistance)
-		//        this.Destroy();
-
-		//    base.Update(gameTime);
-		//}
 	}
 }
