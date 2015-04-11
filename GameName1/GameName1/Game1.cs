68c272adf2d9c47881a7bb609656c9aca945bbc8
GilFerraz <gil.ferraz.gomes@outlook.com>
2015-04-11 01:51:29 +0100

    Revert "Teste"
    This reverts commit 84fe7c014813d20cd6eddd68d8b661b43684b9c0.

---
+++ GameName1/GameName1/Game1.cs
@@ -0,0 +1,79 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+
+namespace GameName1
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D spritesheet;
+        Player candyBitch;
+        public Game1()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            candyBitch = new Player(Content);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+           
+        }
+        
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            //candyBitch.Update(gameTime);
+            base.Update(gameTime);
+        }
+        int contador = 0;
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+
+            candyBitch.Draw(gameTime);
+            if(contador == 700)
+            {
+                candyBitch.Update(gameTime);
+                contador = 0;
+            }
+            contador++;
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
