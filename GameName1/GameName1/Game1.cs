9bf590fb89abdafb7779cbe30557aa68f242b290
teresa1 <teresasofialoureiro@hotmail.com>
2015-05-19 19:41:56 +0100

    menos cenas chatas

--- GameName1/GameName1/Game1.cs
+++ GameName1/GameName1/Game1.cs
@@ -30,13 +30,11 @@ namespace Sugar_Run
         float enemyTime;
         // Power-Ups
         int randomLollipop;
-<<<<<<< HEAD
 
-=======
+
         SpriteFont font;
        
-        
->>>>>>> origin/fixJumps
+   
         // Construtor
         public Game1() : base()
         {
@@ -71,11 +69,8 @@ namespace Sugar_Run
             //scene.AddPlatform(new Plataform(Content));
             player = new Player(Content, "CandyGirl");
             scene.AddSprite(player);
-<<<<<<< HEAD
-=======
 
             font = Content.Load<SpriteFont>("SpriteFont1");
->>>>>>> origin/fixJumps
 
             // Backgrounds
             background = new ScrollingBackground(Content, "Backgrounds/Sky", 0f);
